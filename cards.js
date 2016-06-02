e79696027ba5bc6a39cbf0ded743c88a20be2f97
Glenn Spinner <glenngineer1@gmail.com>
2016-06-01 20:55:37 -0500

    still hung up on delete function

---
+++ cards.js
@@ -0,0 +1,31 @@
+var newSubmit = document.getElementById("submit");
+var newCards = document.getElementById("cards");
+var inputText = document.getElementById("input");
+var counter = 1;
+
+newSubmit.addEventListener("click", createCard);
+// document.body.addEventListener("click", function(event){
+//   console.log(event.target)
+//   console.log(event)
+// });
+
+function createCard() {
+  var outputEl = inputText.value;
+
+  newCards.innerHTML +=
+  '<div id="card-' + counter + '" class="cardDimensions">' +
+    '<div class="output">' + outputEl + '</div>' +
+    '<button class="buttonLocation" type="submit">Delete</button>' +
+  '</div>';
+
+  var thisCard = document.getElementById(`card-${counter}`);
+  thisCard.addEventListener("click", deleteCard);
+  counter++;
+  // console.log(thisCard);
+};
+
+function deleteCard(event) {
+  newCards.removeChild(event.currentTarget);
+}
+
+
