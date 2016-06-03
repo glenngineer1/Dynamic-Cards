ca08858249a6ccf71fd08e4076eedbd57d193c63
Glenn Spinner <glenngineer1@gmail.com>
2016-06-03 14:32:46 -0500

    modified delete, not working but closer?

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 var newSubmit = document.getElementById("submit");
 var newCards = document.getElementById("cards");
 var inputText = document.getElementById("input");
-var counter = 1;
+// var counter = 1;
 
 newSubmit.addEventListener("click", createCard);
 // document.body.addEventListener("click", function(event){
@@ -13,20 +13,21 @@ function createCard() {
   var outputEl = inputText.value;
 
   newCards.innerHTML +=
-  '<div id="card-' + counter + '" class="cardDimensions">' +
+  '<div id="card" class="cardDimensions">' +
     '<div class="output">' + outputEl + '</div>' +
-    '<button class="buttonLocation" type="submit">Delete</button>' +
+    '<button id="button" class="buttonLocation" type="submit">Delete</button>' +
   '</div>';
 
-  var thisCard = document.getElementById(`card-${counter}`);
+  var thisCard = document.getElementById("button");
+  console.log("thisCard", thisCard);
   thisCard.addEventListener("click", deleteCard);
-  counter++;
+  // counter++;
   // console.log(thisCard);
+  function deleteCard() {
+    newCards.parentNode.removeChild(newCards);
+  }
 };
 
-function deleteCard(event) {
-  newCards.removeChild(event.currentTarget);
-}
 
 
 
